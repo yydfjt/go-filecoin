c1a53fd26a93bfeb1a3753a5d0ba47c8ec112108
Travis Person <travisperson@users.noreply.github.com>
2019-02-11 20:41:07 -0800

    Spelling

--- tools/fast/series/series_sendfilecoindefaults.go
+++ tools/fast/series/series_sendfilecoindefaults.go
@@ -8,7 +8,7 @@ import (
 )
 
 // SendFilecoinDefaults sends the `value` amount of fil from the default wallet
-// address of the `from` ndoe to the `to` node's default wallet.
+// address of the `from` node to the `to` node's default wallet.
 func SendFilecoinDefaults(ctx context.Context, from, to *fast.Filecoin, value int) error {
 	var toAddr address.Address
 	if err := to.ConfigGet(ctx, "wallet.defaultAddress", &toAddr); err != nil {
